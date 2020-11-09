f3dc659dadf12c1984326dbcc775c9ed174f7552
Blake Dixon <tbdixon@gmail.com>
2020-11-09 09:08:58 -0500

    Refactoring node implementation

--- rudis/src/bin/main.rs
+++ rudis/src/bin/main.rs
@@ -1,9 +1,7 @@
-use rudis::rudis_node::{IpAddr, NodeType, RudisNode};
+use rudis::rudis_node::RudisNode;
 
 fn main() {
-    let ip_addr = "127.0.0.1";
-    let ports = vec![8080, 8081, 8082, 8083, 8084, 8085];
-    let ip_addr = IpAddr { ip: ip_addr.to_string(), port: 8080 };
-    let n = RudisNode { id: String::from("abcd"), addr: ip_addr, node_type: NodeType::Master };
-    println!("{:?}", n);
+    let ips = vec!["127.0.0.1:8080", "127.0.0.1:8081", "127.0.0.1:8082", "127.0.0.1:8083", "127.0.0.1:8084", "127.0.0.1:8085"];
+    let node = RudisNode::new(ips[0].to_string());
+    println!("{:?}", node);
 }
--- rudis/src/rudis_node.rs
+++ rudis/src/rudis_node.rs
@@ -1,25 +1,46 @@
 use std::collections::HashMap;
+use std::iter;
+use rand::{Rng, thread_rng};
+use rand::distributions::Alphanumeric;
+
 type NodeID = String;
+type IpAddr = String;
 
 #[derive(Debug)]
-pub struct IpAddr {
-    pub ip: String,
-    pub port: u16,
-}
-
-#[derive(Debug)]
-pub enum NodeType {
-    Master,
-    Slave,
+enum NodeType {
+    Primary,
+    Replicas,
+    Solo, 
 }
 
 #[derive(Debug)]
 pub struct RudisNode {
-    pub id: NodeID,
-    pub addr: IpAddr,
-    pub node_type: NodeType,
-/*    slaves: Vec<String>,
+    id: NodeID,
+    ip_addr: IpAddr,
+    node_type: NodeType,
+    replicas: Vec<String>,
     buckets: Vec<u16>,
     cluster_nodes: HashMap<NodeID, RudisNode>,
-    cluster_buckets: HashMap<u16, NodeID>,*/
+    cluster_buckets: HashMap<u16, NodeID>,
+}
+
+impl RudisNode {
+    pub fn new(ip_addr: IpAddr) -> RudisNode {
+        let mut node = RudisNode {
+            id: String::new(),
+            ip_addr: ip_addr,
+            node_type: NodeType::Solo,
+            replicas: Vec::new(),
+            buckets: Vec::new(),
+            cluster_nodes: HashMap::new(),
+            cluster_buckets: HashMap::new(),
+        };
+        node.set_node_id();
+        node
+    }
+    fn set_node_id(&mut self) {
+        let mut rng = thread_rng();
+        self.id = iter::repeat(()).map(|()| rng.sample(Alphanumeric)).take(32).collect();
+        println!("{} : {}", self.ip_addr, self.id);
+    }
 }
