0dbd7d816cc70abd6914cae354448545b48f37ad
Blake Dixon <tbdixon@gmail.com>
2020-11-09 20:44:59 -0500

    Basic threading for two ports

--- rudis/src/bin/main.rs
+++ rudis/src/bin/main.rs
@@ -1,12 +1,14 @@
 use rudis::rudis_node::RudisNode;
 use std::env;
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    let node = RudisNode::new(args);
+    let node = RudisNode::new(args)?;
     println!(
         "Created a new node with address {} and ID {}. Listening for commands",
-        node.socket_addr, node.id
+        node.client_socket_addr, node.id
     );
     node.listen();
+    Ok(())
 }
--- rudis/src/rudis_node.rs
+++ rudis/src/rudis_node.rs
@@ -1,9 +1,11 @@
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 use std::collections::HashMap;
+use std::error::Error;
 use std::io::prelude::*;
 use std::iter;
 use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::thread;
 type NodeID = String;
 
 #[derive(Debug)]
@@ -16,8 +18,10 @@ enum NodeType {
 #[derive(Debug)]
 pub struct RudisNode {
     pub id: NodeID,
-    pub socket_addr: SocketAddr,
-    tcp_listener: TcpListener,
+    pub client_socket_addr: SocketAddr,
+    node_socket_addr: SocketAddr,
+    client_tcp_listener: TcpListener,
+    node_tcp_listener: TcpListener,
     node_type: NodeType,
     replicas: Vec<String>,
     buckets: Vec<u16>,
@@ -26,12 +30,18 @@ pub struct RudisNode {
 }
 
 impl RudisNode {
-    pub fn new(args: Vec<String>) -> RudisNode {
-        let socket_addr = args[1].parse::<SocketAddr>().unwrap();
+    pub fn new(args: Vec<String>) -> Result<RudisNode, Box<dyn Error>> {
+        let client_socket_addr = args[1].parse::<SocketAddr>()?;
+        let mut node_socket_addr = args[1].parse::<SocketAddr>()?;
+        node_socket_addr.set_port(client_socket_addr.port() + 10000);
+        let client_tcp_listener = TcpListener::bind(client_socket_addr.to_string())?;
+        let node_tcp_listener = TcpListener::bind(node_socket_addr.to_string())?;
         let mut node = RudisNode {
             id: String::new(),
-            socket_addr: socket_addr,
-            tcp_listener: TcpListener::bind(socket_addr.to_string()).unwrap(),
+            client_socket_addr: client_socket_addr,
+            node_socket_addr: node_socket_addr,
+            client_tcp_listener: client_tcp_listener,
+            node_tcp_listener: node_tcp_listener,
             node_type: NodeType::Solo,
             replicas: Vec::new(),
             buckets: Vec::new(),
@@ -39,21 +49,46 @@ impl RudisNode {
             cluster_buckets: HashMap::new(),
         };
         node.set_node_id();
-        node
+        Ok(node)
     }
 
     pub fn listen(&self) {
-        for stream in self.tcp_listener.incoming() {
-            let stream = stream.unwrap();
-            let mut buffer = [0; 512];
-            self.parse_request(stream, &mut buffer);
-            println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-            // parse buffer here
-        }
+        thread::spawn(|| {
+            for stream in self.client_tcp_listener.incoming() {
+                self.listen_loop(stream.unwrap()).unwrap();
+            }
+        });
+        /*    thread::spawn(|| {
+            for stream in self.node_tcp_listener.incoming() {
+                self.listen_loop(stream.unwrap()).unwrap();
+            }
+        });*/
     }
 
-    fn parse_request(&self, mut stream: TcpStream, buffer: &mut [u8]) {
-        stream.read(buffer).unwrap();
+    pub fn node_listen(&self) -> Result<(), Box<dyn Error>> {
+        for stream in self.node_tcp_listener.incoming() {
+            self.listen_loop(stream?)?;
+        }
+        Ok(())
+    }
+
+    fn listen_loop(&self, stream: TcpStream) -> Result<(), Box<dyn Error>> {
+        let mut buffer = String::new();
+        self.parse_request(stream, &mut buffer)?;
+        println!("Request: {}", buffer);
+        self.process_request(&buffer);
+        Ok(())
+    }
+
+    fn process_request(&self, request: &String) {}
+
+    fn parse_request(
+        &self,
+        mut stream: TcpStream,
+        buffer: &mut String,
+    ) -> Result<(), Box<dyn Error>> {
+        stream.read_to_string(buffer)?;
+        Ok(())
     }
 
     fn set_node_id(&mut self) {
